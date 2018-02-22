8225aff738d041511e1849ae82a6d0f8f4094fc9
edisch <elijahdisch@gmail.com>
2018-02-22 23:09:23 +0900

    Added success stories and footer elements

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,19 @@
 import $ from 'jquery';
 
 $('.nav__flyout-open, .nav__flyout-close').click(function () {
-  $('.nav__flyout').toggleClass('nav__flyout--active');
+  var $flyout = $('.nav__flyout');
+
+  if ($flyout.is('.nav__flyout--active')) {
+    $flyout
+    .removeClass('nav__flyout--active')
+    .attr('tabindex', '-1')
+    .delay(400)
+    .hide(0);
+  } else {
+    $flyout
+    .show(0)
+    .delay(100)
+    .addClass('nav__flyout--active')
+    .attr('tabindex', '0');
+  }
 });
